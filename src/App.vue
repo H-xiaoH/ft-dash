44e66ab5a7ce5411c71eeea5a5958d8e92751e28
H-xiaoH <40495719+H-xiaoH@users.noreply.github.com>
2026-09-12 13:02:22 +0800

    ✨:feat wire the shell to the router

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '@/stores/auth'
+import AppShell from '@/components/AppShell.vue'
+
+const auth = useAuthStore()
+const { authenticated } = storeToRefs(auth)
+</script>
+
+<template>
+  <AppShell v-if="authenticated">
+    <RouterView />
+  </AppShell>
+  <RouterView v-else />
+</template>
