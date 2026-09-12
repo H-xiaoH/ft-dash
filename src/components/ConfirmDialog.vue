ed0a3609cc7fc47d34b05a00483a42700a6877e6
H-xiaoH <40495719+H-xiaoH@users.noreply.github.com>
2026-09-12 19:46:24 +0800

    🔥:remove the duplicated scoped style

--- src/components/ConfirmDialog.vue
+++ src/components/ConfirmDialog.vue
@@ -74,13 +74,3 @@ watch(
   </Teleport>
 </template>
 
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 160ms var(--ease);
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
