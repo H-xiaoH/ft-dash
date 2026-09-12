d452e93bec2b64d6119c2124eb607d298a67902b
H-xiaoH <40495719+H-xiaoH@users.noreply.github.com>
2026-09-12 13:02:21 +0800

    ✨:feat add trading pair label

---
+++ src/components/PairPill.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from 'vue'
+import { pairBase, pairQuote } from '@/utils/format'
+
+const props = defineProps({
+  pair: { type: String, default: '' },
+  short: { type: Boolean, default: false },
+})
+
+const base = computed(() => pairBase(props.pair))
+const quote = computed(() => pairQuote(props.pair))
+</script>
+
+<template>
+  <span class="pair-pill">
+    <span class="pair-base">{{ base }}</span>
+    <span v-if="!short && quote" class="pair-quote">/{{ quote }}</span>
+  </span>
+</template>
